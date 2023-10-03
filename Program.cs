8a6d18f8238e6ff218c039f017e540417ce3bdc7
user <user@LAPTOP-2DK3J1EI>
2023-10-03 11:45:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Años bisiestos desde 1900:");
+for (int i = 1900; i <= 2023; i++)
+{
+    if (i % 4 == 0 && i % 100 != 0) //bisiesto multiplo de 4
+    {
+        Console.WriteLine(i);
+    }
+    if (i % 400 == 0) // bisiesto multiplo de 400
+    {
+        Console.WriteLine(i);
+    }
+}
